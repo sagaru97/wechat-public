f5a8f92886fe92591a5464814039c102b17e9e1c
sagaru <60592627+sagaru97@users.noreply.github.com>
2023-07-09 14:36:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEHGM6N5IbfpU_A1sYBgwS5531rY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '049OXkA7t12bRXdIGyB9vEVNlOec0p4aOWP5rrvAwrI',
+      useTemplateId: 'D_wx3COdLsQVN648ZUOPVIvrk8X7CYht5_duznHUgM0',
       
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-11',
