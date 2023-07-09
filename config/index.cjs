6c7b3b2e93989dea4990d5c7fdd1faa5b38f021a
sagaru <60592627+sagaru97@users.noreply.github.com>
2023-07-09 15:24:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,11 +24,20 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEHGM6N5IbfpU_A1sYBgwS5531rY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'uGwbvTVBDGb2dlfn5gBSqFdEIIR-LQr7LpReLzaBog8',
+      useTemplateId: 'VnIrrrmleqr-Z0eD1XDoX3l-vJXyIGZQWaqAFOelqIo',
       
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-11',
       horoscopeDateType: '今日'
+
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '元宝', year: '1997', date: '08-10',
+        },
+        {
+          type: '生日', name: '元宝', year: '1997', date: '09-11',
+        },
     },
   ],
   CUSTOMIZED_DATE_LIST: [
