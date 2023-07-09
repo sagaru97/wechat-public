6d06a1f63994b3baf65be0e34e8c2acc6f137b0b
sagaru <60592627+sagaru97@users.noreply.github.com>
2023-07-09 17:49:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,30 +23,6 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oEHGM6OnMYjdFBZJ-V1iS_98xnTc',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'FGdRD5h0CC2jaYuIpj33GF39bKJHMvytScXkKKY7sNE',
-      
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '09-11',
-      horoscopeDateType: '今日',
-
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '媛宝', year: '1997', date: '08-10',
-        },
-        {
-          type: '生日', name: '媛宝', year: '1997', date: '09-11',
-        },
-      ],
-    },
-    
-    // self
-    {
-      // 想要发送的人的名字
-      name: 'gyh',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEHGM6N5IbfpU_A1sYBgwS5531rY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'FGdRD5h0CC2jaYuIpj33GF39bKJHMvytScXkKKY7sNE',
@@ -65,8 +41,7 @@ const USER_CONFIG = {
         },
       ],
     },
-  ],
- 
+ ],
   CUSTOMIZED_DATE_LIST: [
         // 在一起的日子
         { keyword: 'together_day', date: '2023-05-13' },
