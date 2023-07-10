e6ca632694998154a9e25fa195e058b4f8a1ab68
sagaru <60592627+sagaru97@users.noreply.github.com>
2023-07-10 21:56:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -41,6 +41,29 @@ const USER_CONFIG = {
         },
       ],
     },
+    // 宝
+    {
+      // 想要发送的人的名字
+      name: 'self',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oEHGM6N5IbfpU_A1sYBgwS5531rY',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'FGdRD5h0CC2jaYuIpj33GF39bKJHMvytScXkKKY7sNE',
+      
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '09-11',
+      horoscopeDateType: '今日',
+
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '媛宝', year: '1997', date: '08-10',
+        },
+        {
+          type: '生日', name: '媛宝', year: '1997', date: '09-11',
+        },
+      ],
+    },
  ],
   CUSTOMIZED_DATE_LIST: [
         // 在一起的日子
